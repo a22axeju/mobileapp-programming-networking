9e36bef77b5e2966d971af75e7295eb61e623104
Axel <a22axeju@student.his.se>
2023-05-02 19:00:52 +0200

    La till en ny klass Mountain

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,22 @@
+package com.example.networking;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Mountain {
+    private String name;
+    private String location;
+    @SerializedName("size")
+    private int height;
+
+    public Mountain(String name, String location, int height) {
+        this.name = name;
+        this.location = location;
+        this.height = height;
+    }
+    @Override
+    public String toString() {
+        return "Name: '" + name + '\'' +
+                ", Location: '" + location + '\'' +
+                ", Height: " + height;
+    }
+}
